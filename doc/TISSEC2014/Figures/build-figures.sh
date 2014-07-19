66421249de1058dd71faad0de9a02256ea4b700a
Peter C. Chapin <PChapin@vtc.vsc.edu>
2014-07-19 17:59:09 -0400

    Added the source of the TISSEC paper.

---
+++ doc/TISSEC2014/Figures/build-figures.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pic -t Authorization-Interfaces.pic > Authorization-Interfaces.tex
+pic -t Certificate-Daemon.pic > Certificate-Daemon.tex
+pic -t Certificate-Formats.pic > Certificate-Formats.tex
+pic -t Certificate-Receiver-Flowchart.pic > Certificate-Receiver-Flowchart.tex
+pic -t Post-Format.pic > Post-Format.tex
+pic -t SessionKey-Daemon.pic > SessionKey-Daemon.tex
+pic -t SessionKey-Format.pic > SessionKey-Format.tex
+pic -t SprocketRT-Protocol.pic > SprocketRT-Protocol.tex
